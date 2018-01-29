aa313d4a70804a7149f57b8ff6c0991c039c8915
naveenezone <naveen.paulraj@n4softsol.ca>
2018-01-29 10:47:25 -0700

    Mongo DB and Port for Heroku uopdated

--- server/models/user.js
+++ server/models/user.js
@@ -104,7 +104,7 @@ UserSchema.pre('save', function (next) {
   var user = this;
 
   if (user.isModified('password')) {
-    bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.genSalt(5, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
